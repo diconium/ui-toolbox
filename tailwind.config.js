2402db8e8da77a5662579be2e18bce27f6c54779
Robin Demuth <robin.demuth@diconium.com>
2023-02-01 10:25:56 +0100

    clean up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('./colors.config');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
@@ -10,38 +12,9 @@ module.exports = {
     extend: {
       colors: {
         toolbox: {
-          primary: {
-            DEFAULT: '#F27E19',
-            dominant: '#F27E19',
-            900: '#F27E19',
-            800: '#F27E19',
-            700: '#F27E19',
-            600: '#F27E19',
-            500: '#F27E19',
-            accent: '#F27E19',
-            400: '#F27E19',
-            300: '#F27E19',
-            200: '#F27E19',
-            100: '#F27E19',
-            50: '#F27E19',
-            gradient: '#F27E19',
-          },
-          secondary: {
-            DEFAULT: '#977390',
-            dominant: '#977390',
-            900: '#977390',
-            800: '#977390',
-            700: '#977390',
-            600: '#977390',
-            500: '#977390',
-            accent: '#977390',
-            400: '#977390',
-            300: '#977390',
-            200: '#977390',
-            100: '#977390',
-            50: '#977390',
-            gradient: '#977390',
-          },
+          primary: { ...colors.primary },
+          secondary: { ...colors.secondary },
+          ...colors.additional,
         },
       },
     },
