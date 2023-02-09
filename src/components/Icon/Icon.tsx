4a50a19a7f3e196b5a996b6a854a507f563e56a5
Robin Demuth <robin.demuth@diconium.com>
2023-02-09 12:02:44 +0100

    feature/buttons-support-icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -3,14 +3,15 @@ import Font from './config';
 
 export interface IconProps {
   icon: string;
+  className?: string;
 }
 
 export { Font };
 
 const getUnicode = (key: string) => Font[key as keyof typeof Font] || 'NA';
 
-export function Icon({ icon }: IconProps) {
-  return <span className="toolbox-icons-outlined">{getUnicode(icon)}</span>;
+export function Icon({ icon, className = '' }: IconProps) {
+  return <span className={`toolbox-icons-outlined ${className}`}>{getUnicode(icon)}</span>;
 }
 
 export default Icon;
