0064f6e4e2cbf4e566ce6dddb31fc92eff7e9dae
Robin Demuth <robin.demuth@diconium.com>
2023-02-14 14:30:57 +0100

    added validation

--- src/components/TextField/Template.tsx
+++ src/components/TextField/Template.tsx
@@ -1,15 +1,22 @@
 import React, { PropsWithChildren } from 'react';
 import classNames from 'classnames';
+import { validationToColor } from './TextField';
+
+/**  for tailwind to generate the appropriate classes
+   * Do not remove *
+   text-toolbox-feedback-orange text-toolbox-feedback-red
+*/
 
 export interface Props extends PropsWithChildren {
   label?: string;
   hint?: string;
   disabled?: boolean;
+  validation?: string;
 }
 
 const BASE_TEXT = 'text-xs leading-3 px-3 py-1';
 
-function Template({ label, hint, disabled, children }: Props) {
+function Template({ label, hint, disabled, validation, children }: Props) {
   const mask = classNames(BASE_TEXT, {
     'text-toolbox-neutral-400': !disabled,
     'text-toolbox-neutral-100': disabled,
@@ -17,8 +24,10 @@ function Template({ label, hint, disabled, children }: Props) {
   return (
     <div className="flex flex-col">
       {label && <span className={mask}>{label}</span>}
-      {children}
-      {hint && <span className={mask}>{hint}</span>}
+      <div className="inline-flex items-center">{children}</div>
+      {hint && (
+        <span className={classNames(mask, `text-toolbox-feedback-${validationToColor(validation)}`)}>{hint}</span>
+      )}
     </div>
   );
 }
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames';
 import React from 'react';
+import Icon from '../Icon';
 import Template from './Template';
 
 export interface Props {
@@ -7,30 +9,82 @@ export interface Props {
   value?: string;
   type?: string;
   hint?: string;
+  validation?: string;
   disabled?: boolean;
   onChange?: (change: string) => void;
 }
 
-const BASE_TEMPLATE = `w-full border border-toolbox-neutral-200 
-  px-6 py-3 rounded-2xl max-w-xs
-  text-toolbox-neutral focus:outline-none
-  leading-5 text-sm bg-toolbox-white`;
+const BASE_TEMPLATE = `w-full border-2 border-toolbox-neutral-200 
+  px-6 py-3 rounded-xl max-w-xs
+  text-toolbox-neutral-500 leading-5 text-sm bg-toolbox-white
+  focus:outline-none focus:border-toolbox-primary focus:text-toolbox-neutral`;
 
-function TextField({ placeholder, label, value, type = 'text', hint, disabled = false, onChange = () => {} }: Props) {
+/**  for tailwind to generate the appropriate classes
+   * Do not remove *
+   text-toolbox-feedback-green text-toolbox-feedback-orange text-toolbox-feedback-red
+  border-toolbox-feedback-orange border-toolbox-feedback-red
+  focus:border-toolbox-feedback-orange focus:border-toolbox-feedback-red
+*/
+export const validationToColor = (validation?: string) => {
+  if (validation === 'valid') {
+    return 'green';
+  }
+  if (validation === 'warning') {
+    return 'orange';
+  }
+  if (validation === 'error') {
+    return 'red';
+  }
+  return 'black';
+};
+
+const validationToIcon = (validation?: string) => {
+  if (validation === 'valid') {
+    return 'circle-check';
+  }
+  if (validation === 'warning') {
+    return 'circle-information';
+  }
+  return 'circle-xmark';
+};
+
+function TextField({
+  placeholder,
+  label,
+  value,
+  type = 'text',
+  hint,
+  validation,
+  disabled = false,
+  onChange = () => {},
+}: Props) {
+  const color = validationToColor(validation);
+  const template = classNames(
+    BASE_TEMPLATE,
+    ...[validation && `border-toolbox-feedback-${color} focus:border-toolbox-feedback-${color}`]
+  );
+
   return (
     <Template
       label={label}
       hint={hint}
       disabled={disabled}
+      validation={validation}
     >
       <input
         type={type}
         disabled={disabled}
-        className={BASE_TEMPLATE}
+        className={template}
         placeholder={placeholder}
         value={value}
         onChange={(event) => onChange(event.target.value)}
       />
+      {validation && (
+        <Icon
+          icon={validationToIcon(validation)}
+          className={`text-toolbox-feedback-${color} -ml-10`}
+        />
+      )}
     </Template>
   );
 }
