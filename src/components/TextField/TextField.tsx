76f733d08e758f361c43eeeb91a4409e9199df91
Robin Demuth <robin.demuth@diconium.com>
2023-02-16 08:25:32 +0100

    fixed disabled text color

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -61,7 +61,8 @@ function TextField({
   const isValid = validation === 'valid';
   const template = classNames(
     BASE_TEMPLATE,
-    !validation && 'border-toolbox-neutral-200  text-toolbox-neutral-500',
+    !validation && 'border-toolbox-neutral-200',
+    !validation && !disabled && 'text-toolbox-neutral-500',
     ...[
       validation && !isValid && `border-toolbox-feedback-${color} focus:border-toolbox-feedback-${color}`,
       validation && isValid && 'border-toolbox-neutral-200',
