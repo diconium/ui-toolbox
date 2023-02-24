0726eb91f207a59721eac3c11ff2e41db8d9bb62
robdembitel <robin.demuth@diconium.com>
2023-02-24 13:26:34 +0100

    feature/indicators (#87)

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export interface Props {
+  progress?: number;
+  min?: number;
+  max?: number;
+  animate?: boolean;
+}
+
+const MAX = 100;
+const MIN = 0;
+
+const normalize = (progress: number, min: number, max: number) => {
+  if (progress < min) {
+    return min;
+  }
+  if (progress > max) {
+    return max;
+  }
+  return Math.round(progress);
+};
+
+function ProgressBar({ progress = 0, min = MIN, max = MAX, animate = false }: Props) {
+  const template = classNames([
+    'bg-toolbox-primary h-1 rounded-2xl',
+    { 'motion-safe:animate-pulse': animate },
+  ]);
+  return (
+    <div className="w-full h-1 bg-toolbox-neutral-50 rounded-2xl">
+      <div
+        className={template}
+        style={{ width: `${normalize(progress, min, max)}%` }}
+      />
+    </div>
+  );
+}
+
+export default ProgressBar;
