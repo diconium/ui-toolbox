4afb49080d2a971a484e65f01f1461d500b8a31a
Robin Demuth <robin.demuth@diconium.com>
2023-03-09 13:49:44 +0100

    added left and right variants to top/bottom

--- src/components/Tooltip/Display.tsx
+++ src/components/Tooltip/Display.tsx
@@ -20,13 +20,21 @@ export function Display({ tooltip, side = 'right', className = '' }: Props) {
     side === 'right' && 'right-0 translate-x-full -translate-y-1/2 -mr-6',
     side === 'left' && 'left-0 -translate-x-full -translate-y-1/2 -ml-6',
     side === 'top' && 'left-0 -translate-y-full -mt-3 -ml-6',
+    side === 'top|left' && 'left-0 -translate-y-full -mt-3 -ml-6',
+    side === 'top|right' && 'left-0 -translate-y-full -mt-3 -ml-6',
     side === 'bottom' && 'left-0 translate-y-full -bottom-4 -mr-6',
+    side === 'bottom|left' && 'left-0 translate-y-full -bottom-4 -mr-6',
+    side === 'bottom|right' && 'left-0 translate-y-full -bottom-4 -mr-6',
   ]);
   const arrow = classNames(ARROW_BASE_TEMPLATE, [
     side === 'right' && 'left-0 bottom-4 -translate-y-1/4 -ml-4',
     side === 'left' && 'right-0 bottom-4 -translate-y-1/4 -mr-4 rotate-180',
     side === 'top' && 'left-1/2 bottom-0 -mb-6 translate-x-1/2 -ml-4 -rotate-90',
+    side === 'top|left' && 'left-1/4 bottom-0 -mb-6 translate-x-1/2 -ml-4 -rotate-90',
+    side === 'top|right' && 'right-1/4 bottom-0 -mb-6 translate-x-1/2 -ml-4 -rotate-90',
     side === 'bottom' && 'left-1/2 top-0 -mt-6 translate-x-1/2 -ml-4 rotate-90',
+    side === 'bottom|left' && 'left-1/4 top-0 -mt-6 translate-x-1/2 -ml-4 rotate-90',
+    side === 'bottom|right' && 'right-1/4 top-0 -mt-6 translate-x-1/2 -ml-4 rotate-90',
   ]);
   const isSimple = typeof tooltip === 'string' || tooltip instanceof String;
 
