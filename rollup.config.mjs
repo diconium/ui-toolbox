6aa62400fe841a062eddd6f42e0e10093d3b9a4f
robdembitel <robin.demuth@diconium.com>
2023-03-24 11:02:58 +0100

    chore/font-path-postinstall (#114)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,46 +1,61 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
+import copy from 'rollup-plugin-copy';
 
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from './package.json' assert { type: 'json' };
+
+const POSTCSS_OPTIONS = {
+  config: {
+    path: './postcss.config.js',
+  },
+  extensions: ['.css'],
+  minimize: true,
+  inject: {
+    insertAt: 'top',
+  },
+};
+
+const FONTS = [
+  'styles/fonts/toolbox.ttf',
+  'styles/fonts/toolbox.eot',
+  'styles/fonts/toolbox.woff',
+  'styles/fonts/toolbox.woff2',
+];
+
+const COPY_ASSETS_OPTIONS = {
+  targets: [{ src: FONTS, dest: 'dist/fonts' }],
+};
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss({
-        config: {
-          path: "./postcss.config.js",
-        },
-        extensions: [".css"],
-        minimize: true,
-        inject: {
-          insertAt: "top",
-        },
-      }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(POSTCSS_OPTIONS),
+      copy(COPY_ASSETS_OPTIONS),
     ],
   },
   {
-    input: "dist/esm/types/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/types/src/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.(css|less|scss)$/, "react"],
+    external: [/\.(css|less|scss)$/, 'react'],
   },
 ];
