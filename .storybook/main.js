2c0a9ed637aa3f337bb7b1d08e9c266be41b39af
Robin Demuth <robin.demuth@diconium.com>
2023-04-13 09:44:23 +0200

    update to storybook v7, still wip -> need code snippets

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
@@ -9,6 +8,13 @@ module.exports = {
   ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: { fastRefresh: true, lazyCompilation: true },
+    options: {
+      fastRefresh: true,
+      lazyCompilation: true,
+    },
+  },
+  docs: {
+    autodocs: true,
+    defaultName: 'Overview',
   },
 };
