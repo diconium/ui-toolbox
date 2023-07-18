6af7b0858eb0f5b8f5b47e279a180deda4f4cd87
5041A <sofia.lapteva@diconium.com>
2023-07-18 09:40:18 +0200

    chore/increase-code-coverage

--- src/components/SideNavigation/SideNavigation.test.tsx
+++ src/components/SideNavigation/SideNavigation.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { SideNavigation as IMPORT } from '../../index';
 import SideNavigation from './SideNavigation';
+import userEvent from '@testing-library/user-event';
 
 describe('SideNavigation component', () => {
   test('can be imported from the library correctly', () => {
@@ -37,5 +38,50 @@ describe('SideNavigation component', () => {
     expect(screen.getByText(/test-top/i)).toBeInTheDocument();
     expect(screen.getByText(/test-center/i)).toBeInTheDocument();
     expect(screen.getByText(/test-bottom/i)).toBeInTheDocument();
+  });
+
+  test('closes correctly when already opened and clicked', async () => {
+    const user = userEvent.setup();
+    const onClose = jest.fn();
+    const { container } = render(
+      <SideNavigation
+        opened={true}
+        top={<span>test-top</span>}
+        bottom={<span>test-bottom</span>}
+        onOpen={onClose}
+        onClose={onClose}
+      >
+        test-center
+      </SideNavigation>
+    );
+    expect(onClose).not.toHaveBeenCalled();
+
+    const item = container.querySelector('button');
+    if (item) {
+      await act(() => user.click(item));
+    }
+    expect(onClose).toHaveBeenCalled();
+  });
+
+  test('expands correctly when clicked', async () => {
+    const user = userEvent.setup();
+    const onOpen = jest.fn();
+    const { container } = render(
+      <SideNavigation
+        opened={false}
+        top={<span>test-top</span>}
+        bottom={<span>test-bottom</span>}
+        onOpen={onOpen}
+        onClose={onOpen}
+      >
+        test-center
+      </SideNavigation>
+    );
+    expect(onOpen).not.toHaveBeenCalled();
+    const item = container.querySelector('button');
+    if (item) {
+      await act(() => user.click(item));
+    }
+    expect(onOpen).toHaveBeenCalled();
   });
 });
