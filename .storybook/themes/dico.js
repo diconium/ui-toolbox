33346151b6dd390fdab2187901cf41eefd29001a
Sofia <135241329+5041A@users.noreply.github.com>
2023-09-26 07:57:22 +0200

    chore/implement-checklist-for-open-source-prep (#398)

--- .storybook/themes/dico.js
+++ .storybook/themes/dico.js
@@ -1,17 +1,18 @@
 import { create } from '@storybook/theming';
 const colors = require('../../colors.config');
+const logo = require('./diconium.png');
 
 export default create({
   base: 'light',
   fontBase: 'Poppins, sans-serif',
   textColor: colors.neutral.DEFAULT,
-  
+
   colorPrimary: colors.primary.DEFAULT,
   colorSecondary: colors.primary.DEFAULT,
-  
+
   brandTitle: 'diconium',
-  brandUrl: 'https://jobs.diconium.com/',
-  brandImage: 'https://jobs.embitel.de/uploads/1306/settings/companies/diconium-96-63c512c9b9afb.png',
+  brandUrl: 'https://diconium.com/',
+  brandImage: logo,
   brandTarget: '_blank',
 
   appBg: colors.additional.white,
