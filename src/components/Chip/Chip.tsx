82a343162d8409d4628f1a3b33d09fe723e13dac
umerfarooq-diconium <umer.farooq@diconium.com>
2024-03-05 15:19:16 +0100

    update chip component using div instead of button

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -10,7 +10,7 @@ export interface Props {
 
 const BASE_TEMPLATE = `text-sm leading-5 bg-toolbox-white
 rounded-2xl border text-toolbox-black whitespace-nowrap
-px-10 py-1 font-thin`;
+px-10 py-1 font-thin  inline-block`;
 
 function Chip({ label, selected = false, disabled = false, onClick = () => {} }: Props) {
   const template = classNames(BASE_TEMPLATE, {
@@ -20,16 +20,21 @@ function Chip({ label, selected = false, disabled = false, onClick = () => {} }:
     'cursor-not-allowed': disabled,
     'cursor-pointer': !disabled,
   });
-  
-return (
-    <button
-      type="button"
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") onClick();
+  };
+
+  return (
+    <div
       className={template}
-      disabled={disabled}
-      onClick={() => onClick()}
+      role='button'
+      onClick={disabled ? undefined : onClick}
+      tabIndex={0}
+      onKeyDown={disabled ? undefined : handleKeyDown}
     >
       {label}
-    </button>
+    </div>
   );
 }
 
